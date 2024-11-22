0cdd2e7fb7102ad763093df769ed3b837653fcd1
ShamsaKanwal2007 <shamsakanwal2027@gmail.com>
2024-11-22 19:34:52 +0500

    Add files via upload

---
+++ Simple_calculator.py
@@ -0,0 +1,77 @@
+print("Simple Calculator")
+num1 = int (input("enter your first number:"))
+num2 = int (input("enter your second number:"))
+print("operation types:")
+print("+ for Addition")
+print("- for Subtraction")
+print("* for Multiplication")
+print("/ for Division")
+operator = input("Please select any one of above option.")
+result = 30
+if operator == "+":
+    print(num1+num2)
+elif operator == "-":
+    print(num1-num2)
+elif operator == "*":
+    print(num1*num2)
+elif operator == "/":
+    print(num1/num2)
+elif operator:
+    print("Invalid input")
+num1 = int (input("enter your first number:"))
+num2 = int (input("enter your second number:"))
+print("operation types:")
+print("+ for Addition")
+print("- for Subtraction")
+print("* for Multiplication")
+print("/ for Division")
+operator = input("Please select any one of above option.")
+result = 30
+if operator == "+":
+    print(num1+num2)
+elif operator == "-":
+    print(num1-num2)
+elif operator == "*":
+    print(num1*num2)
+elif operator == "/":
+    print(num1/num2)
+elif operator:
+    print("Invalid input")
+num1 = int (input("enter your first number:"))
+num2 = int (input("enter your second number:"))
+print("operation types:")
+print("+ for Addition")
+print("- for Subtraction")
+print("* for Multiplication")
+print("/ for Division")
+operator = input("Please select any one of above option.")
+result = 30
+if operator == "+":
+    print(num1+num2)
+elif operator == "-":
+    print(num1-num2)
+elif operator == "*":
+    print(num1*num2)
+elif operator == "/":
+    print(num1/num2)
+elif operator:
+    print("Invalid input")
+num1 = int (input("enter your first number:"))
+num2 = int (input("enter your second number:"))
+print("operation types:")
+print("+ for Addition")
+print("- for Subtraction")
+print("* for Multiplication")
+print("/ for Division")
+operator = input("Please select any one of above option.")
+result = 30
+if operator == "+":
+    print(num1+num2)
+elif operator == "-":
+    print(num1-num2)
+elif operator == "*":
+    print(num1*num2)
+elif operator == "/":
+    print(num1/num2)
+elif operator:
+    print("Invalid input")
